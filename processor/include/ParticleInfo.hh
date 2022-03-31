af160901cc63b191258155551519bc5e89e9c05f
alexhxia <alexia.hocine@protonmail.com>
2022-03-31 12:22:06 +0200

    first upgrade ParticleInfo

--- processor/include/ParticleInfo.hh
+++ processor/include/ParticleInfo.hh
@@ -1,22 +1,51 @@
+/******************************************************************************/
+/**                                                                          **/
+/**               Team : FCC, IP2I, UCBLyon 1, France, 2022                  **/
+/**                                                                          **/
+/******************************************************************************/
+
 #pragma once
 
-#include <fastjet/PseudoJet.hh>
 
-#include <EVENT/ReconstructedParticle.h>
+#include <fastjet/PseudoJet.hh> // FastJet : softwore package for jet
+#include <EVENT/ReconstructedParticle.h> // LCIO
 
-class ParticleInfo : public fastjet::PseudoJet::UserInfoBase
-{
-  public:
-    ParticleInfo() = default;
-    ~ParticleInfo() = default;
+/**
+ * ParticleInfo class, add to 'fastjet::PseudoJet::UserInfoBase' interface 
+ * once object of 'EVENT::ReconstructedParticle'.
+ */
+class ParticleInfo : public fastjet::PseudoJet::UserInfoBase {
+  
+    public:
+        // MAKERS
+        ParticleInfo() = default;
+        ParticleInfo(const ParticleInfo& toCopy) = delete; 
+        
+        // WRECKER
+        ~ParticleInfo() = default;
 
-    ParticleInfo(const ParticleInfo& toCopy) = delete;
-    void operator=(const ParticleInfo& toCopy) = delete;
+        // REQUESTS
+        EVENT::ReconstructedParticle* getReconstructedParticle() const { 
+            return _recoParticle; 
+        }
+        
+        // COMMANDS
+        void operator=(const ParticleInfo& toCopy) = delete;
 
-    void setRecoParticle(EVENT::ReconstructedParticle* recoPart) { _recoParticle = recoPart; }
+        void setReconstructedParticle(EVENT::ReconstructedParticle* rPart) { _
+            recoParticle = rPart; 
+        }
+        
+        // ------ BEGIN à supprimer ------ /*
+        EVENT::ReconstructedParticle* recoParticle() const { 
+            return _recoParticle; 
+        }
+        void setRecoParticle(EVENT::ReconstructedParticle* rPart) { _
+            recoParticle = rPart; 
+        }
+        // ------ END à supprimer ------*/
 
-    auto recoParticle() const { return _recoParticle; }
-
-  protected:
-    EVENT::ReconstructedParticle* _recoParticle = nullptr;
+    protected:
+        // ATTRIBUTES
+        EVENT::ReconstructedParticle* recoParticle = nullptr;
 };
