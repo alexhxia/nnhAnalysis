97a6b9154c1f0e7b66c132c13ef37b0292fcb0c3
Alexia <47657664+alexhxia@users.noreply.github.com>
2022-04-26 20:28:15 +0200

    Update analysis_launchBDT.sh

--- script/analysis_launchBDT.sh
+++ script/analysis_launchBDT.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-#
-#SBATCH --job-name=launchBDT
-#SBATCH --output=launchBDT_submit.out
-#SBATCH --error=launchBDT_submit.err
-#
-#SBATCH --ntasks=1
-#SBATCH --time=5:00:00          # means 5h 00m 00s
-#SBATCH --mem-per-cpu=1G
-# mail-type=BEGIN, END, FAIL, REQUEUE, ALL, STAGE_OUT, TIME_LIMIT_90
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=a.hocine@ip2i.in2p3.fr
 
 function print_export {
     echo
