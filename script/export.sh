64a306b2f81575592426f6d3beff47c1b3d89609
Alexia <47657664+alexhxia@users.noreply.github.com>
2022-04-26 20:29:09 +0200

    Update export.sh

--- script/export.sh
+++ script/export.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-#
-#SBATCH --job-name=test
-#SBATCH --output=submit.out
-#SBATCH --error=submit.err
-#
-#SBATCH --ntasks=1
-#SBATCH --time=2:00:00          # means 2h 00m 00s
-#SBATCH --mem-per-cpu=1G
-# mail-type=BEGIN, END, FAIL, REQUEUE, ALL, STAGE_OUT, TIME_LIMIT_90
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=a.hocine@ip2i.in2p3.fr
 
 home=~/nnhAnalysis
 branch=ilcsoft
