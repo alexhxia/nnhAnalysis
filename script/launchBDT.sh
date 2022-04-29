6031a309f54cdc092f72494971b6bd506c982668
alexhxia <alexia.hocine@protonmail.com>
2022-04-29 17:11:19 +0200

    April 29th

--- script/launchBDT.sh
+++ script/launchBDT.sh
@@ -91,6 +91,6 @@ if [ conda -eq 0 ]; then
     conda activate env_root_python
 fi
 
-python3 launchBDT_bb.py 1> $NNH_HOME/analysis/DATA/bb_std_output.txt 2> $NNH_HOME/analysis/DATA/bb_error_output.txt 
-python3 launchBDT_WW.py 1> $NNH_HOME/analysis/DATA/WW_std_output.txt 2> $NNH_HOME/analysis/DATA/WW_error_output.txt 
+python3 launchBDT_bb.py 1> $NNH_HOME/analysis/DATA/launchBDT_bb.out 2> $NNH_HOME/analysis/DATA/launchBDT_bb.err 
+python3 launchBDT_WW.py 1> $NNH_HOME/analysis/DATA/launchBDT_WW.err 2> $NNH_HOME/analysis/DATA/launchBDT_WW.err 
 
