08f59a3ed9ede1de6e67bfa0db9b3d156f7dfa5d
alexhxia <alexia.hocine@protonmail.com>
2022-05-05 13:43:49 +0200

    May 5th

--- python/test_2Processors.py
+++ python/test_2Processors.py
@@ -4,12 +4,14 @@
 Ce programme compare les résultats de 2 dossiers de sortie du programme 'processor'.
 """
 
+import numpy
 import argparse 
 import os, os.path 
 import sys 
 import ROOT 
-
-from ROOT import TFile, TH1F, TTree
+import warnings
+#warnings.filterwarnings("ignore", category=RuntimeWarning) 
+from ROOT import TCanvas, TFile, TH1F, TTree
 
 def error(msg):
     """Print error messenger and Stop programme with error"""
@@ -26,8 +28,47 @@ def testInputDirectory(directory):
     if not os.path.isdir(directory):
         error('ERROR : directory is not ' + directory)
 
+def isSameTTree(tree1, tree2):
+    """Test if 2 root trees are same with Kolmogorov definition."""
+    
+    #w = 600;
+    #h = 600;
+    #canvas = TCanvas("canvas", "canvas", w, h);
+    
+    isEgalProcessus = True
+    
+    branchs1 = tree1.GetListOfBranches()
+    branchs2 = tree2.GetListOfBranches()
+    branchs = set(branchs1 + branchs2)
+    print(str(branchs))
+    
+    for branch in branchs:
+    
+        nameBranch = branch1.GetName()
+        branch2 = tree2.GetBranch(nameBranch)
+        tree1.Draw(nameBranch)
+        htemp=ROOT.gPad.GetPrimitive("htemp")
+        xAxis=htemp.GetXaxis()
+        xmin=xAxis.GetXmin()
+        xmax=xAxis.GetXmax()
+        del htemp
+    
+        hist1 = TH1F("hist1", nameBranch, 200, xmin, xmax)
+        hist2 = TH1F("hist2", nameBranch, 200, xmin, xmax)
+    
+        tree1.Draw(nameBranch + ">>hist1")
+        tree2.Draw(nameBranch + ">>hist2")
+    
+        k = hist1.KolmogorovTest(hist2, "UON")
+        if not k == 1.:
+            print(nameBranch + ": kolmogorov test = " + str(k))
+
+        del hist1
+        del hist2
 
 if __name__ == "__main__":
+    
+    # PARAMETERS
     
     parser = argparse.ArgumentParser()
     
@@ -49,6 +90,8 @@ if __name__ == "__main__":
     p2Directory = args['processor2']
     testInputDirectory(p2Directory)
     
+    # FOR ALL PROCESSUS
+    
     numProcessus = [
         402173, 402182, 402007, 402008, 402176, 402185, 402009, 402010, 402011, 
         402012, 402001, 402002, 402013, 402014, 402003, 402004, 402005, 402006, 
@@ -60,45 +103,60 @@ if __name__ == "__main__":
         500126, 500127, 500128
     ]
     
-    #for numP in numProcessus:
-    numP = 402173
-    numP = str(numP) + ".root"
-    path_p1 = os.path.join(p1Directory, numP)
-    path_p2 = os.path.join(p2Directory, numP)
-    if not os.path.exists(path_p1) or not os.path.exists(path_p2) :
-        print(numP + "no exist for 2")
-    else :
-        print(numP)
-        file1 = TFile.Open(path_p1)
-        file2 = TFile.Open(path_p2)
-        
-        tree1 = file1.Get("tree")
-        tree2 = file2.Get("tree")
-        
-        branchs1 = tree1.GetListOfBranches()
-        for branch1 in branchs1:
-        
-            nameBranch = branch1.GetName()
-            #nameBranch = "mc_nu_e"
-            branch2 = tree2.GetBranch(nameBranch)
-        
-            tree1.Draw(nameBranch)
-            tree2.Draw(nameBranch)
-        
-            hist1 = TH1F("hist1", nameBranch, 200, 0, 200)
-            hist2 = TH1F("hist2", nameBranch, 200, 0, 200)
-        
-            tree1.Draw(nameBranch + ">>hist1")
-            tree2.Draw(nameBranch + ">>hist2")
-        
-            k = hist1.KolmogorovTest(hist2, "UON")
-            print(nameBranch + ": kolmogorov test = " + str(k))
-
-            del hist1
-            del hist2
-
-        file1.Close()
-        file2.Close()
+    # add num processus if is missing
+    processusMissing1 = []
+    processusMissing2 = []
     
-
-    print("----- END -----")
+    # for processus 1 and 2 are different tree
+    processusDistinct = []
+    
+    for numP in numProcessus:
+        
+        numPFileName = str(numP) + ".root"
+        path_p1 = os.path.join(p1Directory, numPFileName)
+        path_p2 = os.path.join(p2Directory, numPFileName)
+        
+        if not os.path.exists(path_p1) or not os.path.exists(path_p2):
+            
+            if not os.path.exists(path_p1):
+                processusMissing1.apprend(numP)
+            
+            if not os.path.exists(path_p2):
+                processusMissing2.apprend(numP)
+            
+            print("\n" + numP + "no exist for 2")
+            
+        else :
+            
+            # FOR ALL TREE
+            
+            file1 = TFile.Open(path_p1)
+            file2 = TFile.Open(path_p2)
+            
+            tree1 = file1.Get("tree")
+            tree2 = file2.Get("tree")
+            
+            if not isSameTTree(tree1, tree2):
+                processusDistinct.append(numP)
+            
+            file1.Close()
+            file2.Close()
+    
+    # OUTPUT
+    
+    if len(processusMissing1) == 0:
+        print("Processus1 is complete.")
+    else:
+        print("Processus Missing for -p1:\n" + str(processusMissing1))
+        
+    if len(processusMissing2) == 0:
+        print("Processus2 is complete.")
+    else:
+        print("Processus Missing for -p2:\n" + str(processusMissing2))
+    
+    if len(processusDistinct) == 0:
+        print("Processus1 and Processus are same.")
+    else:
+        print("Processus1 and Processus are distinct" + str(processusDistinct))
+    
+    print("\n----- END TEST_2PROCESSOR -----\n")
