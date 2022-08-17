281edfaaef352e5d06198f386021a6bad0e04ab5
alexhxia <alexia.hocine@protonmail.com>
2022-08-17 20:33:31 +0200

    August 17th, 2022: export upgrade

--- nnhScript/nnhExport.sh
+++ nnhScript/nnhExport.sh
@@ -1,28 +1,49 @@
 #!/bin/bash
 
+source tools/functions.sh
+source tools/help.sh
+
 ################################## ATTRIBUTES ##################################
 
 # SERVER
+
 server=/gridgroup/ilc/nnhAnalysisFiles # path to server
 
 input=$server/AHCAL # path to data
 output=$server/result # path to result
 
-# PROJET
+# LOCAL
+
+## PROJET
+
 path=~/nnhAnalysis # path to projet
 
-## SUB-PROJET
 program=$path/nnhProgram # path to programs 
 result=$path/nnhResult # path to results
 script=$path/nnhScript # path to script for run program easier
 test=$path/nnhTest # path to result file tests
 
 ### HOME
+
 original=$program/original
 ilcsoft=$program/ilcsoft
 fcc=$program/fcc
 
-home=""
+#branch=original
+branch=ilcsoft # default
+#branch=fcc
+
+home=$program/$branch
+
+#### PROCESSOR
+
+p_input=$input
+p_output=$home/processor/RESULTS # default, local
+
+#### ANALYSIS
+
+a_input=$p_output
+a_output=$home/analysis/DATA # default, local
 
 ################################### COMMANDS ###################################
 
@@ -46,7 +67,9 @@ function setServerOutput {
     output=$1
 }
 
-# PROJET
+# LOCAL
+
+## PROJECT
 
 function setPath {
     test_isValidPath $1
@@ -57,8 +80,6 @@ function setPath {
     setScript $path/nnhScript
     setTest $path/nnhTest
 }
-
-# SUB-PROJET
 
 function setProgram {
     test_isValidPath $1
@@ -84,9 +105,42 @@ function setTest {
     test=$1
 }
 
+## HOME
+
+function setBranch {
+    branch=$1
+    
+    setHome $program/$branch
+}
+
 function setHome {
     test_isValidHome $1
     home=$1
+    
+    setProcessorOutput $home/processor/RESULTS
+    setAnalysisOutput $home/analysis/DATA
+}
+
+### PROCESSOR
+
+function setProcessorInput {
+    p_input=$1
+}
+
+function setProcessorOutput {
+    p_output=$1
+
+    setAnalysisInput $p_output
+}
+
+### ANALYSIS
+
+function setAnalysisInput {
+    a_input=$1
+}
+
+function setAnalysisOutput {
+    a_output=$1
 }
 
 # Add or Update environment variables
@@ -113,6 +167,14 @@ function nnh_export {
     
     ## HOME
     export NNH_HOME=$home
+    
+    ### PROCESSOR
+    export NNH_PROCESSOR_INPUT=$p_input
+    export NNH_PROCESSOR_OUTPUT=$p_output
+    
+    ### ANALYSIS
+    export NNH_ANALYSIS_INPUT=$a_input
+    export NNH_ANALYSIS_OUTPUT=$a_output
 }
 
 ################################### REQUESTS ###################################
@@ -132,12 +194,17 @@ function print_variables {
     echo "- script:         $script"
     echo "- test:           $test"
     echo
-    echo "choices for home path :"
-    echo "- original:       $original"
-    echo "- ilcsoft:        $ilcsoft"
-    echo "- fcc:            $fcc"
-    echo
     echo "home:             $home"
+    echo "- path:           $path"
+    echo "- branch:         $branch"
+    echo
+    echo "processor:"
+    echo "- input:          $p_input"
+    echo "- output:         $p_output"
+    echo
+    echo "analysis:"
+    echo "- input:          $a_input"
+    echo "- output:         $a_output"
     echo
 }
 
@@ -145,61 +212,86 @@ function print_export {
     echo 
     echo "Environment variables : "
     echo
-    echo "NNH_SERVER:       $NNH_SERVER"
-    echo "NNH_INPUT:        $NNH_INPUT"
-    echo "NNH_OUTPUT:       $NNH_OUTPUT"
+    echo "NNH_SERVER:               $NNH_SERVER"
+    echo "NNH_INPUT:                $NNH_INPUT"
+    echo "NNH_OUTPUT:               $NNH_OUTPUT"
     echo
-    echo "NNH_PATH:         $NNH_PATH"
+    echo "NNH_PATH:                 $NNH_PATH"
     echo 
-    echo "NNH_PROGRAM:      $NNH_PROGRAM"
-    echo "NNH_RESULT:       $NNH_RESULT"
-    echo "NNH_SCRIPT:       $NNH_SCRIPT"
-    echo "NNH_TEST:         $NNH_TEST"
+    echo "NNH_PROGRAM:              $NNH_PROGRAM"
+    echo "NNH_RESULT:               $NNH_RESULT"
+    echo "NNH_SCRIPT:               $NNH_SCRIPT"
+    echo "NNH_TEST:                 $NNH_TEST"
     echo
-    echo "NNH_ORIGINAL:     $NNH_ORIGINAL"
-    echo "NNH_ILCSOFT:      $NNH_ILCSOFT"
-    echo "NNH_FCC:          $NNH_FCC"
+    echo "NNH_ORIGINAL:             $NNH_ORIGINAL"
+    echo "NNH_ILCSOFT:              $NNH_ILCSOFT"
+    echo "NNH_FCC:                  $NNH_FCC"
     echo
-    echo "NNH_HOME:         $NNH_HOME"
+    echo "NNH_HOME:                 $NNH_HOME"
+    echo 
+    echo "NNH_PROCESSOR_INPUT:      $NNH_PROCESSOR_INPUT"
+    echo "NNH_PROCESSOR_OUTPUT:     $NNH_PROCESSOR_OUTPUT"
+    echo 
+    echo "NNH_ANALYSIS_INPUT:       $NNH_ANALYSIS_INPUT"
+    echo "NNH_ANALYSIS_OUTPUT:      $NNH_ANALYSIS_OUTPUT"
     echo
 }
 
 ################################ REQUESTS: TEST ################################
 
 # Test if the directory home is valid
+# Entry: $1, a path like $path
 function test_isValidPath {
-    if ! [ -d $path ]; then 
+    if ! [ -d $1 ]; then 
         echo "Path $path is not find"
         exit 1
     fi
 }
 
-# Test if the directory home is valid
+# Stop a program if the directory home or program directory are not find
+# Entry: $1, a path like /$path/$branch
 function test_isValidHome {
+    
     if ! [ -d $1 ]; then 
         echo "home $1 directory no exist"
         exit 1
-    elif ! [ -d $1/analysis ]; then 
-        echo 'analysis directory no exist'
-        exit 1
-    elif ! [ -d $1/processor ]; then 
-        echo 'processor directory no exist'
-        exit 1
-    fi
+    fi 
+    
+    shopt -s nocasematch
+
+    case $1 in
+
+        $NNH_ORIGINAL | $NNH_ILCSOFT)
+            if ! [ -d $1/analysis ]; then 
+                echo 'No containt analysis directory'
+                exit 1
+            elif ! [ -d $1/processor ]; then 
+                echo 'No containt processor directory'
+                exit 1
+            fi
+            ;;
+
+        $NNH_FCC)
+            ;;
+
+        *)
+            echo "$1"
+            echo "WARNING: the path is valid, but the sub-directories can't check."
+            ;;
+    esac
 }
 
 # Test if name project exist (no use now)
 # Entry : $1, a name branch 
-branchsValid=(original ilcsoft fcc)
 function test_isValidBranch {
     valid=false
-    for b in "${branchsValid[@]}" ; do
-        if [ $b == $1 ] ; then
+    for b in $NNH_PROGRAM/*; do
+        echo "$b"
+        if [ -d $NNH_PROGRAM/$1 ] && [ $b == $NNH_PROGRAM/$1 ] ; then
             valid=true 
         fi
     done
     if ! $valid ; then
-        echo "-b $branch"
-        exit 1
+        error "-b $branch"
     fi
 }
